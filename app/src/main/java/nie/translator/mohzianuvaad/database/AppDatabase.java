462f3356e4f0736f0e7488ab79ac02c20034a377
devesh711 <pdevesh699@gmail.com>
2025-03-21 03:38:31 +0530

    ADD Database module

---
+++ app/src/main/java/nie/translator/mohzianuvaad/database/AppDatabase.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2025 Devesh Patel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copyFile of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nie.translator.mohzianuvaad.database;
+
+import androidx.room.RoomDatabase;
+import nie.translator.mohzianuvaad.database.dao.MyDao;
+import nie.translator.mohzianuvaad.database.entities.RecentPeerEntity;
+
+
+@androidx.room.Database(version = 1, entities = {RecentPeerEntity.class})
+abstract public class AppDatabase extends RoomDatabase {
+    abstract public MyDao myDao();
+}
